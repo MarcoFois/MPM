37511a51f304fd27edef9c6b12414c27cf56bdeb
Carlo de Falco <carlo.defalco@polimi.it>
2023-05-16 14:28:18 +0200

    esempio fena

---
+++ data/fena/input_fena.m
@@ -0,0 +1,130 @@
+
+clear all;
+close all;
+clc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rowstart = 550;
+rowend   = 900;
+
+colstart = 171;
+colend   = 430;
+
+ndivrows = rowend - rowstart;
+ndivcols = colend - colstart;
+
+hx = hy = 5;
+immagine = imread ("dtm_liguria_2017.tif", "PixelRegion", {[rowstart rowend], [colstart, colend]});
+immagine_int = reshape (typecast (immagine(end:-1:1, :), "int16"), size (immagine));
+immagine_int(immagine_int<0) = -1;
+
+y = linspace (0, hy*ndivrows, ndivrows+1);
+x = linspace (0, hx*ndivcols, ndivcols+1);
+
+[X, Y] = meshgrid (x, y);
+Z = double (immagine_int);
+
+h = 0*Z;
+Ymin = 1500; Ymax = 1600; Xmin = 700; Xmax = 900;
+select = (Y>Ymin & Y<Ymax & X >Xmin & X < Xmax);
+
+h(select)= 101 - Z(select);
+h(h < 0) = 0;
+
+DX = 1; DY = 1;
+[xp, yp] = meshgrid (Xmin:DX:Xmax, Ymin:DY:Ymax);
+hp = interp2 (X, Y, h, xp, yp, 'spline');
+hp = hp(:);
+xp = xp(:);
+yp = yp(:);
+
+hzero = find (hp <= 1);
+hp(hzero) = [];
+xp(hzero) = [];
+yp(hzero) = [];
+
+%data = jsonencode (struct ('X', X(:), 'Y', Y(:), 'Z', Z(:), 'h', h(:)));
+immagine_out = uint16(immagine_int);
+imwrite (immagine_out, "sfondo.tif")
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+Zz        = double (immagine_int);
+Zz(Zz<=0) = -1.0;
+[gx, gy]  = gradient (Zz);
+
+Z    = Zz(:);
+dZdx = gx(:);
+dZdy = gy(:);
+
+
+figure()
+mesh(X,Y,Zz)
+
+figure()
+scatter3(xp(:),yp(:),hp(:))
+
+
+%% Constants
+g     = 9.81;
+xi    = 200;
+vis   = 50; 
+ty    = 2000;
+T     = 10;
+
+%% Material point quantities initialization
+nmp   = numel(xp);
+rhosy = 1100.0;
+Msys  = sum (hp*DX*DY*rhosy);
+Mp    = Msys/nmp * ones(nmp, 1);
+Vp    = Mp./rhosy;
+Ap    = Vp./hp;
+vp    = zeros (nmp,2); 
+
+momp  = zeros (nmp,2);
+
+Fb(:,1) = zeros (nmp,1);
+Fb(:,2) = zeros (nmp,1);
+
+%%
+DATA = struct (
+	   "x", xp, ...
+	   "y", yp, ...
+	   "Mp", Mp, ...
+	   "Ap", Ap, ...
+	   "vpx", vp(:,1), ...
+	   "vpy", vp(:,2), ...
+	   "Nex", ndivcols, ...
+	   "Ney", ndivrows, ...
+	   "hx", hx, ...
+	   "hy", hy, ...
+	   "hp", hp, ...
+	   "mom_px", momp(:,1), ...
+	   "mom_py", momp(:,2), ...
+	   "g", g, ...
+	   "T", T, ...
+	   "xi", xi, ...
+	   "vis", vis, ...
+	   "ty", ty, ...
+	   "rho", rhosy, ...
+	   "Vp", Vp, ...
+	   "Z", Z, ...
+	   "dZdx", dZdx,
+	   "dZdy", dZdy
+	 );
+json = jsonencode(DATA);
+
+FID = fopen("DATA.json","w");
+fprintf(FID,json);
+fclose(FID);
+
+
+
+
+
+
+
+
+
